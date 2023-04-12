e92283b08c7a3382f0db35102d05cdf1f52f0d9d
simon mor <simomor@gmail.com>
2023-04-12 13:21:49 +0300

    debounce

---
+++ src/utils/debounce.ts
@@ -0,0 +1,17 @@
+type Func = (...args: any[]) => any;
+
+export default function debounce<F extends Func>(func: F, delay: number): (...args: Parameters<F>) => void {
+    let timeoutId: ReturnType<typeof setTimeout> | null;
+
+    return function debounce(this: any, ...args: Parameters<F>) {
+        const context = this;
+
+        if (timeoutId !== null) {
+            clearTimeout(timeoutId);
+        }
+
+        timeoutId = setTimeout(() => {
+            func.apply(context, args);
+        }, delay);
+    }
+}
